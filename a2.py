f6a50b4ef48126c8ef8b393d3c70b8e4efe700a6
Ritzy22 <43897207+Ritzy22@users.noreply.github.com>
2023-10-14 12:55:09 +1100

    Update a2.py

--- a2.py
+++ a2.py
@@ -44,8 +44,8 @@ class A2Topology( Topo ):
         
         # Set BW
         self.addLink( s1, s2, bw=12 ) # high bw = 2-4Mbps per user (video + voice)
-        self.addLink( s1, s3, bw=0.3,delay='5ms', jitter='5ms', loss=2) # poor bw = 100kbps (min) per user (voice)
-        self.addLink( s1, s4, bw=3 ,delay='0.5ms', jitter='2ms', loss=0.5) # low bw = 1Mbps per user (datanetwork)
+        self.addLink( s1, s3, bw=0.3,delay='5ms',jitter='5ms',loss=2) # poor bw = 100kbps (min) per user (voice)
+        self.addLink( s1, s4, bw=3,delay='0.5ms',jitter='2ms',loss=0.5) # low bw = 1Mbps per user (datanetwork)
 
 
 topos = {
